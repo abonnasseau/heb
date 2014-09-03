84d4108538116540acd0c79999cb427ce90dfe08
Lexman <musclean@lexman.org>
2014-09-03 23:13:31 +0200

    improvements in install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 # usage :
-# wget https://raw.githubusercontent.com/abonnasseau/heb/master/install_kernel.sh
-# chmod +x install_kernel.sh
-# ./install_kernel.sh
+# wget https://raw.githubusercontent.com/abonnasseau/heb/master/install_kernel.sh ; chmod +x install_kernel.sh ; ./install_kernel.sh
 
 echo "deb http://ftp.debian.org/debian/ wheezy-backports main non-free contrib" >> /etc/apt/sources.list
 
@@ -13,3 +11,4 @@ apt-get -t wheezy-backports install linux-image-amd64 linux-headers-amd64
 
 apt-get install 
 
+reboot
